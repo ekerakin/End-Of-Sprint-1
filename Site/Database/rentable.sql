bce1594f34f4027dacbda6e645462c1f3f9636b2
ekerakin <ekerakin@outlook.com>
2014-04-03 00:08:15 +0300

    first commit

---
+++ Site/Database/rentable.sql
@@ -0,0 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.8.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Anamakine: localhost
+-- Üretim Zamanı: 29 Mar 2014, 23:16:25
+-- Sunucu sürümü: 5.5.34-0ubuntu0.13.04.1
+-- PHP Sürümü: 5.4.9-4ubuntu2.4
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Veritabanı: `rentable`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `Book`
+--
+
+CREATE TABLE IF NOT EXISTS `Book` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `abstract` varchar(255) DEFAULT NULL,
+  `author` tinytext,
+  `publisher` tinytext,
+  `subject` tinytext,
+  `ranting` int(10) DEFAULT NULL,
+  `link` varchar(150) DEFAULT NULL,
+  `Languageid` int(10) NOT NULL,
+  `UserId` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKBook121789` (`Languageid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `category`
+--
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) DEFAULT NULL,
+  `Bookid` int(10) NOT NULL,
+  `Languageid` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKcategory556514` (`Languageid`),
+  KEY `FKcategory40710` (`Bookid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `country`
+--
+
+CREATE TABLE IF NOT EXISTS `country` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `countries` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `Genre`
+--
+
+CREATE TABLE IF NOT EXISTS `Genre` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `category` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `Language`
+--
+
+CREATE TABLE IF NOT EXISTS `Language` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `language` tinytext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `MovieCD`
+--
+
+CREATE TABLE IF NOT EXISTS `MovieCD` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `ranting` int(10) DEFAULT NULL,
+  `director` tinytext,
+  `writer` tinytext,
+  `description` varchar(255) DEFAULT NULL,
+  `country` int(10) DEFAULT NULL,
+  `runtime` int(10) DEFAULT NULL,
+  `premier` int(10) DEFAULT NULL,
+  `link` varchar(150) DEFAULT NULL,
+  `Languageid` int(10) NOT NULL,
+  `Genreid` int(10) NOT NULL,
+  `UserId` int(10) NOT NULL,
+  `countryid` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKMovieCD865838` (`countryid`),
+  KEY `FKMovieCD949815` (`Genreid`),
+  KEY `FKMovieCD578119` (`Languageid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `MusicCD`
+--
+
+CREATE TABLE IF NOT EXISTS `MusicCD` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `album` tinytext,
+  `year` int(10) DEFAULT NULL,
+  `language` tinytext,
+  `publisher` tinytext,
+  `singer` tinytext,
+  `runtime` int(10) DEFAULT NULL,
+  `link` varchar(150) DEFAULT NULL,
+  `Languageid` int(10) NOT NULL,
+  `Typeid` int(10) NOT NULL,
+  `UserId` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKMusicCD39017` (`Typeid`),
+  KEY `FKMusicCD575529` (`Languageid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `Type`
+--
+
+CREATE TABLE IF NOT EXISTS `Type` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dökümü yapılmış tablolar için kısıtlamalar
+--
+
+--
+-- Tablo kısıtlamaları `Book`
+--
+ALTER TABLE `Book`
+  ADD CONSTRAINT `FKBook121789` FOREIGN KEY (`Languageid`) REFERENCES `Language` (`id`);
+
+--
+-- Tablo kısıtlamaları `category`
+--
+ALTER TABLE `category`
+  ADD CONSTRAINT `FKcategory40710` FOREIGN KEY (`Bookid`) REFERENCES `Book` (`id`),
+  ADD CONSTRAINT `FKcategory556514` FOREIGN KEY (`Languageid`) REFERENCES `Language` (`id`);
+
+--
+-- Tablo kısıtlamaları `MovieCD`
+--
+ALTER TABLE `MovieCD`
+  ADD CONSTRAINT `FKMovieCD578119` FOREIGN KEY (`Languageid`) REFERENCES `Language` (`id`),
+  ADD CONSTRAINT `FKMovieCD865838` FOREIGN KEY (`countryid`) REFERENCES `country` (`id`),
+  ADD CONSTRAINT `FKMovieCD949815` FOREIGN KEY (`Genreid`) REFERENCES `Genre` (`id`);
+
+--
+-- Tablo kısıtlamaları `MusicCD`
+--
+ALTER TABLE `MusicCD`
+  ADD CONSTRAINT `FKMusicCD575529` FOREIGN KEY (`Languageid`) REFERENCES `Language` (`id`),
+  ADD CONSTRAINT `FKMusicCD39017` FOREIGN KEY (`Typeid`) REFERENCES `Type` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Site/jsFiles/CreateItem.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+function check(){
+	var page=document.getElementById("selectType");
+	if(chosedOption.value=="Musics"){
+		window.open("../htmlFiles/CreateItem-Musics.hmtl");
+		}
+	window.onload()=check();
+	}
